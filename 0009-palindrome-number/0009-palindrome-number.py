1f7f9a242dd1458aaa387b9d9d0f50a83524176f
Mauricio Rey Hernández Curiel <mau.rhcl@gmail.com>
2026-08-26 09:39:11 -0600

    Time: 9 ms (64.31%), Space: 12.4 MB (18.56%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -2,11 +2,6 @@ class Solution(object):
     def isPalindrome(self, x):
         if x < 0:
             return False
-        dummy_x = x
-        rev_x = 0
-        while x:
-            rev_x = (rev_x * 10) + x % 10
-            x //= 10
-        return rev_x == dummy_x
+        return x == int(str(x)[::-1])
 
         
