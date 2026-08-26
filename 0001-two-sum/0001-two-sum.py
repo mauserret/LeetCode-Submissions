fb3fd0f1433a12e056c8a837e11792f197cb5c02
Mauricio Rey Hernández Curiel <mau.rhcl@gmail.com>
2026-08-26 17:17:33 -0600

    Time: 0 ms (100%), Space: 13.2 MB (36.55%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,31 @@
 class Solution(object):
     def twoSum(self, nums, target):
+        """
         remain = {}
         for i, num in enumerate(nums):
             if num in remain:
                 return [remain[num], i]
             remain[target-num] = i
-        
+        """
+        """
+        n = len(nums)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
+        """
+        """
+        loop i through nums
+            loop j through from i to the end of nums
+                if nums[i] + nums [j] is target 
+                then return [i,j]
+        """
+        table = {}
+        for i in range(len(nums)):
+            
+            if nums[i] in table:
+                return [table[nums[i]], i]
+            remain = target - nums[i]
+            table[remain] = i
+
         
