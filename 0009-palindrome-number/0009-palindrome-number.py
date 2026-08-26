dc3e2d4a1b41c1e012de6c2975c74f8d7a83b11a
Mauricio Rey Hernández Curiel <mau.rhcl@gmail.com>
2026-08-26 09:55:14 -0600

    Time: 10 ms (60.14%), Space: 12.3 MB (90.4%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -12,12 +12,6 @@ class Solution(object):
         """
         if x < 0 or (x % 10 == 0 and x != 0):
             return False
-        d_x = x
-        rev_x = 0
-        while d_x:
-            rev_x = rev_x * 10 + d_x % 10
-            d_x //= 10
-        return x == rev_x
-
+        return x == int(str(x)[::-1])
 
         
