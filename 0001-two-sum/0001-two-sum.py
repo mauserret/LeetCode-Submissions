1b13bffa3b2df24ac5f7b8818cb12731b3b086f7
Mauricio Rey Hernández Curiel <mau.rhcl@gmail.com>
2026-08-27 17:10:58 -0600

    Time: 2 ms (56.97%), Space: 13.3 MB (16.79%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -14,8 +14,10 @@ class Solution(object):
                 if nums[i] + nums[j] == target:
                     return [i,j]
         """
+        comp = {}
         for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+            complement = target - nums[i]
+            if nums[i] in comp:
+                return [comp[nums[i]], i]
+            comp[complement] = i
                 
